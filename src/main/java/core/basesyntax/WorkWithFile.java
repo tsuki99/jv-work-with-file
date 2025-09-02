0687dad4d306a505cb8542cf363b827cee950601
Vlad Rublenko <vladik.rar@ukr.net>
2025-09-03 00:27:50 +0300

    Added implementation of getStatistic method to process CSV data and generate report

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,52 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithFile {
+    private static final String COMMA = ",";
+    private static final String BUY = "buy";
+    private static final String SUPPLY = "supply";
+    private static final int INITIAL_SUM = 0;
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        int sumBuy = INITIAL_SUM;
+        int sumSupply = INITIAL_SUM;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFileName))) {
+            String value = bufferedReader.readLine();
+
+            while (value != null) {
+                String[] data = value.split(COMMA);
+                int number = Integer.parseInt(data[1]);
+
+                if (value.contains(BUY)) {
+                    sumBuy += number;
+                } else if (value.contains(SUPPLY)) {
+                    sumSupply += number;
+                }
+
+                value = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read the file", e);
+        }
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(toFileName))) {
+            bufferedWriter.write(getReport(sumBuy, sumSupply));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to the file");
+        }
+    }
+
+    private String getReport(int sumBuy, int sumSupply) {
+        int result = sumSupply - sumBuy;
+
+        return SUPPLY + COMMA + sumSupply + System.lineSeparator()
+                + BUY + COMMA + sumBuy + System.lineSeparator()
+                + "result" + COMMA + result;
     }
 }
